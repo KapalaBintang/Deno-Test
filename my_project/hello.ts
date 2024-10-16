f2301521d717d63c761c421c2dc86e21b5cfc250
Abdul Aziz <kapalabintang@gmail.com>
2024-10-16 09:24:48 +0700

    creating server

--- my_project/hello.ts
+++
@@ -1,5 +0,0 @@
-const greet = (name: string) => {
-  return `Hello ${name}!`;
-};
-
-console.log(greet("Deno 2.💥"));
--- my_project/main_test.ts
+++ my_project/main_test.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "@std/assert";
+import { expect } from "jsr:@std/expect";
 import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+Deno.test("Simpel test", () => {
+  const result = add(9, 2);
+  expect(result).toEqual(11);
+
+  assertEquals(add(6, 5), 8);
 });
